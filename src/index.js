c7ffd157865194c8d1cb412f1df7db19948c40c4
dogan-keles <131661707+dogan-keles@users.noreply.github.com>
2023-08-16 19:35:19 +0300

    validasyon tamam gibi

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import PizzaForm from "./components/PizzaForm";
 
 ReactDOM.render(<App />, document.getElementById("root"));
