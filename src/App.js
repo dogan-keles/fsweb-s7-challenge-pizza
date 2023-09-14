3733740a2dd255020076d606524b5b55dae2ed9a
dogan-keles <131661707+dogan-keles@users.noreply.github.com>
2023-09-14 16:13:40 +0300

    vercel-test-commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Success from "./components/Success";
+import Header from "./components/Header";
+import Pizza from "./components/pizza";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./components/Header.css";
+import "./components/Pizza.css";
+import "./components/Footer.css";
 
 const App = () => {
   return (
@@ -10,11 +20,16 @@ const App = () => {
             <Pizza />
           </Route>
 
-  return (
-    <>
-      <h1>Teknolojik Yemekler</h1>
-      <p>Burdaki kodu silip kendi headerınızı ekleyebilirsiniz</p>
-    </>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/success">
+            <Success />
+          </Route>
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
   );
 };
 export default App;
