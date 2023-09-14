6bdb9edfe5d6f76797df7ebf5748ce5069e4fce1
dogan-keles <131661707+dogan-keles@users.noreply.github.com>
2023-09-14 19:06:30 +0300

    sanki-oldu

---
+++ build/precache-manifest.4dbd3bf711cc9d91e4e506fabe2b79ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "74e150fbf8e17d21b424",
+    "url": "/static/css/main.1dc40453.chunk.css"
+  },
+  {
+    "revision": "74e150fbf8e17d21b424",
+    "url": "/static/js/main.7ff7821f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a484b5a1504edf46f1ec",
+    "url": "/static/css/2.c8505dbe.chunk.css"
+  },
+  {
+    "revision": "a484b5a1504edf46f1ec",
+    "url": "/static/js/2.81de3795.chunk.js"
+  },
+  {
+    "revision": "e1143cc0e3fcb007c841e793e7c7047f",
+    "url": "/static/media/mvp-banner.e1143cc0.png"
+  },
+  {
+    "revision": "388294e8cbb0f6405072bfd793aef180",
+    "url": "/index.html"
+  }
+];
